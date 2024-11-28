8697bed207ec76b7e241010a8369b97186a2cc8e
Cristiangonzz <cristianuruuy@gmail.com>
2024-11-27 22:22:47 -0300

    no sql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceModule } from './service/service.module';
+import { PacienteModule } from './componente/paciente/paciente.module';
+import { HomeModule } from './componente/home/home.module';
+import { PacienteRoutingModule } from './componente/paciente/paciente-routing.module';
+import { HomeRoutingModule } from './componente/home/home-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ServiceModule,
+    PacienteModule,
+    HomeModule,
+    PacienteRoutingModule,
+    HomeRoutingModule,
     AppRoutingModule
   ],
   providers: [
